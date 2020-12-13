13bb969fd1a8ccead0780cb8cd4ab4702e1b071c
Julie Jeong <75141596+juliejeong@users.noreply.github.com>
2020-12-13 17:49:29 -0500

    create deployment
    - createPost has some errors

--- frontend/src/CreatePost.tsx
+++ frontend/src/CreatePost.tsx
@@ -1,15 +1,26 @@
 import React, {useState} from 'react';
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
 type Props = {
     email: any
 }
+
+type Song = {
+    readonly title: string;
+    readonly body: string;
+    readonly date: string;
+    readonly type: string;
+    readonly email: string;
+    readonly id: string;
+  };
 
 function CreatePost({email}: Props){
 
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
     const [date, setDate] = useState("");
+    const [posts, setPosts] = useState("");
     
     const updateTitle=(event: React.ChangeEvent<HTMLInputElement>) => {
         const newTitle = event.currentTarget.value;
@@ -23,13 +34,22 @@ function CreatePost({email}: Props){
         const newDate = event.currentTarget.value;
         setDate(newDate);
     }
+    const updatePosts=(event: React.ChangeEvent<HTMLInputElement>) => {
+        const newPost = event.currentTarget.value;
+        setPosts(newPost);
+    }
 
     //function to call create post (takes in title, body, date) 
+    const tempID: string = "-1"
 
-    //have three posts functions, one for each category
-    const createPostAcademics= {
-        //call createPost(title, body, date, type: academics, email )
+    const createPost= async (title: string, body: string, date: string, type: string, email: string, id: string) =>{
+        const postBody: Song = {title, body, date, type, email, id};
+        const newPost = await axios.post<string>('/createPost', postBody);
+       // setPosts([...posts, {title, body, date, type, email, id}]);
     }
+
+
+    /*
     const createPostCareer= {
         //call createPost(title, body, date, type: career)
     }
@@ -37,16 +57,20 @@ function CreatePost({email}: Props){
     const createPostClubs = {
         // call createPost(title, body, date, type: clubs)
     }
-  
-
+    */
 
     return(
         <div>
             <input type="text" placeholder="Title" value={title} onChange={updateTitle}></input><br/>
             <input type="text" placeholder="Body" value={body} onChange={updateBody}></input><br/>
             <input type="text" placeholder="MM/DD/YY" value={date} onChange={updateDate}></input><br/>
-            
-            {email != null ? <div><Button>Post</Button> <br/></div>: <div><br/>You must sign in to post.</div>}
+
+            {email != null ? <div>Post to...<br/></div>: <div><br/>You must sign in to post.</div>}
+
+            {email != null ? <div><Button onClick={createPost(title, body, date, "Academics", email, "-1")}>Academics</Button> <br/></div>: <div></div>}
+            {email != null ? <div><Button>Career</Button> <br/></div>: <div></div>}
+            {email != null ? <div><Button>Clubs</Button> <br/></div>: <div></div>}
+
         </div>
     )
 }
