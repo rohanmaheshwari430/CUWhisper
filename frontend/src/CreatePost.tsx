c692f18621567ccbebd1bda870894f0e6ac84290
Julie Jeong <75141596+juliejeong@users.noreply.github.com>
2020-12-13 19:17:10 -0500

    Create deployment added

--- frontend/src/CreatePost.tsx
+++ frontend/src/CreatePost.tsx
@@ -20,7 +20,6 @@ function CreatePost({email}: Props){
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
     const [date, setDate] = useState("");
-    const [posts, setPosts] = useState("");
     
     const updateTitle=(event: React.ChangeEvent<HTMLInputElement>) => {
         const newTitle = event.currentTarget.value;
@@ -34,10 +33,6 @@ function CreatePost({email}: Props){
         const newDate = event.currentTarget.value;
         setDate(newDate);
     }
-    const updatePosts=(event: React.ChangeEvent<HTMLInputElement>) => {
-        const newPost = event.currentTarget.value;
-        setPosts(newPost);
-    }
 
     //function to call create post (takes in title, body, date) 
     const tempID: string = "-1"
@@ -45,19 +40,7 @@ function CreatePost({email}: Props){
     const createPost= async (title: string, body: string, date: string, type: string, email: string, id: string) =>{
         const postBody: Song = {title, body, date, type, email, id};
         const newPost = await axios.post<string>('/createPost', postBody);
-       // setPosts([...posts, {title, body, date, type, email, id}]);
     }
-
-
-    /*
-    const createPostCareer= {
-        //call createPost(title, body, date, type: career)
-    }
-
-    const createPostClubs = {
-        // call createPost(title, body, date, type: clubs)
-    }
-    */
 
     return(
         <div>
@@ -65,12 +48,11 @@ function CreatePost({email}: Props){
             <input type="text" placeholder="Body" value={body} onChange={updateBody}></input><br/>
             <input type="text" placeholder="MM/DD/YY" value={date} onChange={updateDate}></input><br/>
 
-            {email != null ? <div>Post to...<br/></div>: <div><br/>You must sign in to post.</div>}
+            {email != null ? <div><br/>Post to...<br/></div>: <div><br/>You must sign in to post.</div>}
 
-            {email != null ? <div><Button onClick={createPost(title, body, date, "Academics", email, "-1")}>Academics</Button> <br/></div>: <div></div>}
-            {email != null ? <div><Button>Career</Button> <br/></div>: <div></div>}
-            {email != null ? <div><Button>Clubs</Button> <br/></div>: <div></div>}
-
+            {email != null ? <div><Button onClick={()=>createPost(title, body, date, "Academics", email, tempID)}>Academics</Button> </div>: <div></div>}
+            {email != null ? <div><Button onClick={()=>createPost(title, body, date, "Career", email, tempID)}>Career</Button> </div>: <div></div>}
+            {email != null ? <div><Button onClick={()=>createPost(title, body, date, "Clubs", email, tempID)}>Clubs</Button> </div>: <div></div>}
         </div>
     )
 }
